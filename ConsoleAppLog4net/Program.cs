7fb6e52fed097295cd4849aa7290bdf494110f3f
Rohit <ofd.aws96@gmail.com>
2021-02-10 12:41:22 +0530

    added log4net

--- ConsoleAppLog4net/Program.cs
+++ ConsoleAppLog4net/Program.cs
@@ -1,12 +1,29 @@
-﻿using System;
+﻿using log4net;
+using log4net.Config;
+using System;
+using System.IO;
+using System.Reflection;
 
 namespace ConsoleAppLog4net
 {
     class Program
     {
+        private static readonly ILog log = LogManager.GetLogger(MethodBase.GetCurrentMethod().DeclaringType);
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // Load configuration
+            var logRepository = LogManager.GetRepository(Assembly.GetEntryAssembly());
+            XmlConfigurator.Configure(logRepository, new FileInfo("log4net.config"));
+
+            Console.WriteLine("Hello world!");
+
+            // Log some things
+            log.Info("Hello logging world!");
+            log.Error("Error!");
+            log.Warn("Warn!");
+
+            Console.ReadLine();
         }
     }
 }
